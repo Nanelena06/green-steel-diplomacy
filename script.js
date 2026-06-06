99dabb8fcbaab0752e320113c634806b941ec4c7
Nanelena06 <anahelenagomes85@gmail.com>
2026-06-06 18:17:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        toggle.innerHTML = "☀️";
+    }else{
+        toggle.innerHTML = "🌙";
+    }
+
+});
